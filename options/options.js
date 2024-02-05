99a9633fe6120a61c6a02c93f83bf2b99d980074
Crowdbotics <team@crowdbotics.com>
2024-02-05 22:56:34 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "family-sync-46016",
+  url: "https://family-sync-46016.botics.co",
+  api: "https://family-sync-46016.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
